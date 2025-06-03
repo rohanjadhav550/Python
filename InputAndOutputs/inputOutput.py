f38014d5d5c5d59e5fa589725b281b4f759d2d2f
rohanjadhav550 <rohanjdhv97@gmail.com>
2025-06-03 09:32:09 +0530

    Inputs

--- InputAndOutputs/inputOutput.py
+++ InputAndOutputs/inputOutput.py
@@ -1,25 +1,30 @@
-name = input('What is your name:')
+# name = input('What is your name:')
 
-print('Hello!',name,' welcome to python')
-a,b = input('\nGive us 2 mubers:').split()
+# print('Hello!',name,' welcome to python')
+# a,b = input('\nGive us 2 mubers:').split()
 
-print('\nThese are the 2 numbers you have given: ',a,b)
+# print('\nThese are the 2 numbers you have given: ',a,b)
 
-operator = 0
-while(operator == 0):
-    operator = input('\nTell us what operation do we have to do with this number:')
-    a=int(a)
-    b=int(b)
-    if operator == '+':
-        print('Sum is: ',a+b)
-    elif operator == '-':
-        print('Difference is:',a-b)
-    elif operator == '*' or operator == 'x':
-        print('Product is:', a*b)
-    elif operator == '/':
-        print('Division is:',a/b)
-    else:
-        print('Invalide operator, you must choose any one of +,-,*,/ only')
-        operator = 0
+# operator = 0
+# while(operator == 0):
+#     operator = input('\nTell us what operation do we have to do with this number:')
+#     a=int(a)
+#     b=int(b)
+#     if operator == '+':
+#         print('Sum is: ',a+b)
+#     elif operator == '-':
+#         print('Difference is:',a-b)
+#     elif operator == '*' or operator == 'x':
+#         print('Product is:', a*b)
+#     elif operator == '/':
+#         print('Division is:',a/b)
+#     else:
+#         print('Invalide operator, you must choose any one of +,-,*,/ only')
+#         operator = 0
 
-print('Good bye!')
+# print('Good bye!')
+
+# Multiple inputs and type convertion
+
+[a, b, c] = [int(x) for x in input('Enter 3 numbers: ').split()]
+print(f'Numbers are {a}, {b} and {c}')
